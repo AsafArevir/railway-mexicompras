3070e16261380e0e10646d3d2451235f1075ffdb
AsafArevir <100048373+AsafArevir@users.noreply.github.com>
2023-05-31 21:46:52 -0600

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -1,6 +1,6 @@
 const DB_HOST = process.env.DB_HOST || 'localhost'
 const DB_USER = process.env.DB_USER || 'root'
-const DB_PASSWORD = process.env.DB_PASSWORD 'Nolose123$'
+const DB_PASSWORD = process.env.DB_PASSWORD || 'Nolose123$'
 const DB_NAME = process.env.DB_NAME || 'mexicompras'
 const DB_PORT = process.env.DB_PORT || 3306
 
